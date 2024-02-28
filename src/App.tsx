944541faac1418b6f669ddf9696df7aa6f9d9315
gabrielfilipedev <gabrielfilipe2346@gmail.com>
2024-02-28 13:57:27 -0300

    finalizando exercicio

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Provider } from 'react-redux'
+import Header from './components/Header'
+import Produtos from './containers/Produtos'
+
+import { store } from './store'
+
+import { GlobalStyle } from './styles'
+
+export type Produto = {
+  produto: any
+  find(arg0: (fav: any) => boolean): unknown
+  filter(arg0: (p: any) => boolean): unknown
+  id: number
+  nome: string
+  preco: number
+  imagem: string
+}
+
+function App() {
+  return (
+    <Provider store={store}>
+      <GlobalStyle />
+      <div className="container">
+        <Header />
+        <Produtos />
+      </div>
+    </Provider>
+  )
+}
+
+export default App
